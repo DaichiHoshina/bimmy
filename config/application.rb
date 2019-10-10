92468c79ce5b147c94be67d2a53d38cca427afe8
lemonuchujin <hoshina0713@yahoo.co.jp>
2019-10-10 22:41:16 +0900

    env設定変更

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
