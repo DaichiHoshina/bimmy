e28484eacc831c7c14edde7bf44050954b15a3d5
lemonuchujin <hoshina0713@yahoo.co.jp>
2019-10-18 18:25:42 +0900

    carrierwaveの設定変更

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Develop
+module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
