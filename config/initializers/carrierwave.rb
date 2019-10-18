cc62706f2fac639cf51119b1bfa1a3911d0d9e93
lemonuchujin <hoshina0713@yahoo.co.jp>
2019-10-18 16:24:34 +0900

    carrierwaveの設定変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-require 'carrierwave/storage/abstract'
+equire 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage :fog
     config.fog_provider = 'fog/aws'
+    config.fog_directory = 'bimmyrails'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'us-east-2'
+      region: 'us-east-2',
+      path_style: true
     }
-    config.fog_directory = 'bimmyrails'
   end
   # 日本語ファイル名の設定
   CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
