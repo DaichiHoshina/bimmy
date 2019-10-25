c5fbc8a01e61fa6cc54a6e3cb870508a44a7c69e
lemonuchujin <hoshina0713@yahoo.co.jp>
2019-10-25 12:04:26 +0900

    人気ランキング機能

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+PER = 5
+
 class RankingsController < ApplicationController
   def index
-    @likes = Post.all
-    @likes_ranking = @likes.page(params[:page]).per(PER).order(created_at: :desc)
+    @likes = Post.find(Like.group(:post_id)
+                 .order('count(post_id) desc')
+                 .limit(5)
+                 .pluck(:post_id))
   end
 end
