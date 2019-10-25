9cc31d17518c60f4807a367a310696a55a1bd07b
lemonuchujin <hoshina0713@yahoo.co.jp>
2019-10-25 11:18:11 +0900

    人気ランキング機能

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class RankingsController < ApplicationController
+  def index
+    @likes = Post.all
+    @likes_ranking = @likes.page(params[:page]).per(PER).order(created_at: :desc)
+  end
 end
