b2583bb0615420e0d6552f4db71c49ef577430fc
lemonuchujin <hoshina0713@yahoo.co.jp>
2019-10-26 14:07:58 +0900

    gem変更

--- Gemfile
+++ Gemfile
@@ -53,7 +53,7 @@ gem 'rspec-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'bullet'
+  # gem 'bullet'
   gem 'factory_bot_rails'
   gem 'pry-byebug'
   gem 'pry-doc'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,13 +14,13 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.after_initialize do
-    Bullet.enable = true # Bulletプラグインを有効
-    Bullet.alert = true # JavaScriptでの通知
-    Bullet.bullet_logger = true # log/bullet.logへの出力
-    Bullet.console = true # ブラウザのコンソールログに記録
-    Bullet.rails_logger = true # Railsログに出力
-  end
+  # config.after_initialize do
+  #   Bullet.enable = true # Bulletプラグインを有効
+  #   Bullet.alert = true # JavaScriptでの通知
+  #   Bullet.bullet_logger = true # log/bullet.logへの出力
+  #   Bullet.console = true # ブラウザのコンソールログに記録
+  #   Bullet.rails_logger = true # Railsログに出力
+  # end
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
