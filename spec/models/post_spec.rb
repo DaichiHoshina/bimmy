fcb0ecc801bb6b6e285263e738fa6f04473fdcea
lemonuchujin <hoshina0713@yahoo.co.jp>
2019-10-30 17:52:18 +0900

    Postモデルのテスト作成

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,50 +3,46 @@
 require 'rails_helper'
 
 describe '投稿機能', type: :model do
-  before do
-    @user = create(:user)
-  end
-
   context '有効な情報が入力されたとき' do
     it '必要なデータが入力され、正常に登録できる' do
-      post = build(:post, user_id: @user.id)
+      post = build(:post)
       expect(post).to be_valid
     end
   end
 
   context '無効な情報が入力されたとき' do
     it '感想がないためエラーになる' do
-      post = build(:post, description: nil, user_id: @user.id)
+      post = build(:post, description: nil)
       expect(post.valid?).to eq(false)
     end
 
     it '画像がないためエラーになる' do
-      post = build(:post, image: nil, user_id: @user.id)
+      post = build(:post, image: nil)
       expect(post.valid?).to eq(false)
     end
 
     it '品名がないためエラーになる' do
-      post = build(:post, title: nil, user_id: @user.id)
+      post = build(:post, title: nil)
       expect(post.valid?).to eq(false)
     end
 
     it '評価がないためエラーになる' do
-      post = build(:post, rate: nil, user_id: @user.id)
+      post = build(:post, rate: nil)
       expect(post.valid?).to eq(false)
     end
 
     it '品名が21文字以上でエラーになる' do
-      post = build(:post, title: ('a' * 21), user_id: @user.id)
+      post = build(:post, title: ('a' * 21))
       expect(post.valid?).to eq(false)
     end
 
     it '感想が201文字以上でエラーになる' do
-      post = build(:post, description: ('a' * 201), user_id: @user.id)
+      post = build(:post, description: ('a' * 201))
       expect(post.valid?).to eq(false)
     end
 
     it '店名が21文字以上でエラーになる' do
-      post = build(:post, store: ('a' * 21), user_id: @user.id)
+      post = build(:post, store: ('a' * 21))
       expect(post.valid?).to eq(false)
     end
   end
