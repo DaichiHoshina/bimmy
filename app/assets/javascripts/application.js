9fa9e965e425eb237f18f99afdd24994265c80d2
lemonuchujin <hoshina0713@yahoo.co.jp>
2019-11-03 14:55:08 +0900

    pagetopボタンを変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
@@ -30,42 +29,6 @@ $(document).ready(function() {
         $('body, html').animate({ scrollTop: 0 }, 500);
         return false;
     });
-});
-
-
-//■page topボタン
-$(function(){
-var topBtn=$('#pageTop');
-topBtn.hide();
-
-
-
-//◇ボタンの表示設定
-$(window).scroll(function(){
-  if($(this).scrollTop()>60){
-
-    //---- 画面を80pxスクロールしたら、ボタンを表示する
-    topBtn.fadeIn();
-
-  }else{
-
-    //---- 画面が80pxより上なら、ボタンを表示しない
-    topBtn.fadeOut();
-
-  }
-});
-
-
-
-// ◇ボタンをクリックしたら、スクロールして上に戻る
-topBtn.click(function(){
-  $('body,html').animate({
-  scrollTop: 0},500);
-  return false;
-
-});
-
-
 });
 
 
